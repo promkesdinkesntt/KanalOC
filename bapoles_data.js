988be8a0d566aa8d1662a63f239779235944e3c3
promkesdinkesntt <promkesdinkesntt2@gmail.com>
2026-09-16 15:19:47 +0800

    Add files via upload

--- bapoles_data.js
+++ bapoles_data.js
@@ -13,20 +13,23 @@ window.BAPOLES_DATA = {
       "kategori": "RSKD Jiwa Naimata",
       "materi": "Manajemen Stres dan Pengenalan Alat HRV",
       "youtubeLink": "https://www.youtube.com/live/GOu-wBBaIcw?si=yXHmHpq7S2WLyOLx",
+      "youtubeId": "GOu-wBBaIcw",
+      "foto": "https://img.youtube.com/vi/GOu-wBBaIcw/hqdefault.jpg",
       "tanggal": "12 Maret 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Psikolog Klinis : Maria Imacumata M.A. Lio Dando, M.Psi.,Psikolog",
         "Perawat Jiwa : Imakulata Bete, S.Kep.,Ns",
         "Host :  Iren Garu"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 2,
       "kategori": "Pekan Podcast, cancer awarness week-NTT",
       "materi": "\"Kanker payudara, katong su tahu ko?\" kupas tuntas kanker payudara\r\nApa itu kanker payudara dan mengapa bisa terjadi?",
       "youtubeLink": "https://www.youtube.com/live/zICnXyGhbx4?si=abpVTAHSWGS5KcuL",
+      "youtubeId": "zICnXyGhbx4",
+      "foto": "https://img.youtube.com/vi/zICnXyGhbx4/hqdefault.jpg",
       "tanggal": "9 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -34,14 +37,15 @@ window.BAPOLES_DATA = {
         "dr. Donals A. Manuain, Sp.Onk.Rad",
         "Joyce Margriet Tibuludji, SKM, M.Kes (Penyintas kanker payudara)",
         "Host : Helen"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 3,
       "kategori": "Pekan Podcast, cancer awarness week-NTT",
       "materi": "Mulai hari ini, sekarang su! Cegah kanker\r\nPencegahan kanker (cara sederhana mencegah kanker sejak dini)",
       "youtubeLink": "https://www.youtube.com/live/tbMIYu6RyHE?si=Mrg9YD7FNPa-Ujoq",
+      "youtubeId": "tbMIYu6RyHE",
+      "foto": "https://img.youtube.com/vi/tbMIYu6RyHE/hqdefault.jpg",
       "tanggal": "10 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -49,14 +53,15 @@ window.BAPOLES_DATA = {
         "dr. Benny Gunawan Sp.B.Subsp.Onk(K).,M.Kes",
         "Yuniansen Imelda F. Mella (Penyintas kanker serviks)",
         "Host : dr. Thamara"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 4,
       "kategori": "Pekan Podcast, cancer awarness week-NTT",
       "materi": "Bagandeng katong lawan kanker: cerita pejuang hebat\r\nTantangan yang dihadapi pasien kanker serta cerita inspiratif dari pejuang kanker",
       "youtubeLink": "https://www.youtube.com/live/hpWJpp4sD5Y?si=YnKVx6-1rMbJ_9Gs",
+      "youtubeId": "hpWJpp4sD5Y",
+      "foto": "https://img.youtube.com/vi/hpWJpp4sD5Y/hqdefault.jpg",
       "tanggal": "13 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -64,14 +69,15 @@ window.BAPOLES_DATA = {
         "Maria Martina H. Darung (YKAI perwakilan NTT)",
         "Thieneke Mesakh (Penyintas kanker payudara)",
         "Host : dr. Norman"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 5,
       "kategori": "Pekan Podcast, cancer awarness week-NTT",
       "materi": "Gizi deng herbal yang karmana untuk lawan kanker\r\nGizi Cerdas dan herbal bijak",
       "youtubeLink": "https://www.youtube.com/live/JAddHTM2Xi8?si=PO3hKHPz28_JIMVV",
+      "youtubeId": "JAddHTM2Xi8",
+      "foto": "https://img.youtube.com/vi/JAddHTM2Xi8/hqdefault.jpg",
       "tanggal": "14 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -79,14 +85,15 @@ window.BAPOLES_DATA = {
         "Dr. apt. Ini Nyoman Yuliani, S.Si.,S.Farm., M.Si",
         "Luisa Bertha Tio (Penyintas kanker payudara)",
         "Host : apt. James E. Ga Bani, S.Farm"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 6,
       "kategori": "Pekan Podcast, cancer awarness week-NTT",
       "materi": "Kanker bukan akhir dari segalanya, katong masih punya harapan\r\nDukungan keluarga dan kualitas hidup pasien",
       "youtubeLink": "https://www.youtube.com/live/lOpLBPT2lqY?si=PTHpd9VCHFmz-1Il",
+      "youtubeId": "lOpLBPT2lqY",
+      "foto": "https://img.youtube.com/vi/lOpLBPT2lqY/hqdefault.jpg",
       "tanggal": "15 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -94,40 +101,43 @@ window.BAPOLES_DATA = {
         "Adriana Luan, S.Kep.,Ns",
         "Yustina Malur, S.K.M., M.Kes (Keluarga penyintas kanker)",
         "Host : Tim P2PTM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 7,
       "kategori": "Tenaga Kesehatan",
       "materi": "SKP aman, Praktik nyaman, Kupas tuntas pemenuhan SKP bagi Named Nakes",
       "youtubeLink": "https://www.youtube.com/live/83Of8HQZj58?si=QFSmIuXxkku1YQH-",
+      "youtubeId": "83Of8HQZj58",
+      "foto": "https://img.youtube.com/vi/83Of8HQZj58/hqdefault.jpg",
       "tanggal": "16 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Joyyce Margriet Tibuludji, SKM, M.Kes (Kabid Tenaga Kesehatan)",
         "Host: Yudustira R. Kikhau, SKM, MKM (Kasie Peningkatan Mutu, Pembinaan dan Pengawasan NAKES)"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 8,
       "kategori": "Laboratorium Kesehatan",
       "materi": "Di balik hasil akurat: cerita dari Laboratorium",
       "youtubeLink": "https://www.youtube.com/live/HOHhMrWAsWA?si=i0LE0WZ5_J_rrm48",
+      "youtubeId": "HOHhMrWAsWA",
+      "foto": "https://img.youtube.com/vi/HOHhMrWAsWA/hqdefault.jpg",
       "tanggal": "20 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Nur Azizah, SKM, MKM",
         "Host: Celestiana A. S. Say, S.Farm"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 9,
       "kategori": "Pelayanan Kesehatan",
       "materi": "Mutu Pelayanan di Fasilitas Pelayanan Kesehatan",
       "youtubeLink": "https://www.youtube.com/live/ZImmMAEdPM8?si=Ez9pAXEI1x4A2s6O",
+      "youtubeId": "ZImmMAEdPM8",
+      "foto": "https://img.youtube.com/vi/ZImmMAEdPM8/hqdefault.jpg",
       "tanggal": "23 April 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -135,164 +145,176 @@ window.BAPOLES_DATA = {
         "Agustina J. Ledoh, S.Si, Apt. M.Kes (Kasie Yankes Rujukan)",
         "Riska Yuniatri (BPJS Cab. Kupang)",
         "Host: Diana E. Lusi"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 10,
       "kategori": "Kesehatan Masyarakat",
       "materi": "Posyandu tangguh, Generasi kuat tanpa stunting",
       "youtubeLink": "https://www.youtube.com/live/wP6z0aWWaKk?si=afsUiYjLcE-3Khfw",
+      "youtubeId": "wP6z0aWWaKk",
+      "foto": "https://img.youtube.com/vi/wP6z0aWWaKk/hqdefault.jpg",
       "tanggal": "4 Mei 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Damiana V. Djahari, SKM., M. Kes",
         "Scherly Hayer, S. Gz",
         "Host: Ni Made Oka Arpini Kasuma, SP., M.Kes"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 11,
       "kategori": "RSKD Jiwa Naimata",
       "materi": "Child Grooming",
       "youtubeLink": "https://www.youtube.com/live/LKIRlcQHSUY?si=BuBrIfMbA1eXUkwR",
+      "youtubeId": "LKIRlcQHSUY",
+      "foto": "https://img.youtube.com/vi/LKIRlcQHSUY/hqdefault.jpg",
       "tanggal": "21 Mei 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Imakulata Bete, S.Kep., NS",
         "Host: Ireni Garu"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 12,
       "kategori": "Pelayanan Kesehatan",
       "materi": "Apoteker Mengawal Obat Aman dan Bermutu Hingga Ke Tangan Anda",
       "youtubeLink": "https://www.youtube.com/live/e5dFZ4ZaMPs?si=MQF_htu62cMMARrQ",
+      "youtubeId": "e5dFZ4ZaMPs",
+      "foto": "https://img.youtube.com/vi/e5dFZ4ZaMPs/hqdefault.jpg",
       "tanggal": "10 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Apt. Theresia Maria Wonga, S.Farm.MHIthEcPol (Apoteker RSUD W.Z Johannes Kupang)",
         "Matthew Rawlins (Senior Pharmacist Antimicrobial Stewardship Fiona Stanley Hospital",
         "Host: Secilia Lindawati Leo, S.Farm."
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 13,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Terapi Pencegahan Tuberkulosis (TPT) untuk NTT Sehat",
       "youtubeLink": "https://youtu.be/A1rh-7IVNbc?si=BnE1yNSg3IX73LYQ",
+      "youtubeId": "A1rh-7IVNbc",
+      "foto": "https://img.youtube.com/vi/A1rh-7IVNbc/hqdefault.jpg",
       "tanggal": "11 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr. Lindayanti Sumali, Sp.P",
         "Meriyanti, S.Kep.Ns",
         "Host: Novita Moeda, S.KM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 14,
       "kategori": "Tenaga Kesehatan",
       "materi": "Peluang Penugasan Khusus ditengah Polemik Regulasi",
       "youtubeLink": "https://www.youtube.com/live/3439wKIWQZ8?si=Rb5vV74_ZHEGdnWw",
+      "youtubeId": "3439wKIWQZ8",
+      "foto": "https://img.youtube.com/vi/3439wKIWQZ8/hqdefault.jpg",
       "tanggal": "15 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Joyce M. Timbuludji, SKM, M.Kes (Kabid Nakes)",
         "Ronald Raya Todoboli, SKM, M.Kes (Kasie P3 Nakes)",
         "Host: Wilan Wilhelmina"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 15,
       "kategori": "Kesehatan Masyarakat",
       "materi": "Bicara Sehat Calon Bunda, Topik: Kupas Tuntas Tentang Kehamilan",
       "youtubeLink": "https://www.youtube.com/live/UuPzamTbGao?si=laTp-cXWvZ4mtx9-",
+      "youtubeId": "UuPzamTbGao",
+      "foto": "https://img.youtube.com/vi/UuPzamTbGao/hqdefault.jpg",
       "tanggal": "17 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr. Erma Rantela'bi.,Sp.OG (Ketua POGI NTT Periode 2025-2028)",
         "Maria Imakulata N Fatrin, S.Tr.Keb (Ketua Timker Maternal Neonatal",
         "Host: Dwi Putri Pandara"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 16,
       "kategori": "RSKD Jiwa Naimata",
       "materi": "Tugas Perkembangan Anak dan Remaja dalam Hubungan dengan Kesehatan Mental",
       "youtubeLink": "https://www.youtube.com/live/OSJsTWf7TUg?si=7BtiiexDfNtX7HKz",
+      "youtubeId": "OSJsTWf7TUg",
+      "foto": "https://img.youtube.com/vi/OSJsTWf7TUg/hqdefault.jpg",
       "tanggal": "18 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Aplonia Nenobais, S.Kep., Ns., M.Kep",
         "Host: Yusarmuni Reinnaty, S.Keb"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 17,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Apa Itu Tuberkulosis (TB) Untuk Masyarakat NTT Yang Sehat",
       "youtubeLink": "https://youtu.be/AHxzMiWTwdE?si=GrlNl2yTBAKTT4dg",
+      "youtubeId": "AHxzMiWTwdE",
+      "foto": "https://img.youtube.com/vi/AHxzMiWTwdE/hqdefault.jpg",
       "tanggal": "25 Juni 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr.Nikson E. Faaot, Sp.P.,Fisr.,F-Onk",
         "Host: Liontin L. Suruk, SKM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 18,
       "kategori": "Sekretariat",
       "materi": "Alur Perencanaan dan Penganggaran Dinas Kesehatan Provinsi NTT",
       "youtubeLink": "https://youtu.be/PPNcwUEWuG4?si=v5A5xE0u7VhTBSEP",
+      "youtubeId": "PPNcwUEWuG4",
+      "foto": "https://img.youtube.com/vi/PPNcwUEWuG4/hqdefault.jpg",
       "tanggal": "7 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Hendro Sius Wila Buky, S.KM, M.KM",
         "Marcelinus Pratama Atasoge, SKM",
         "Host: Dwi M. Susanto, SKM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 19,
       "kategori": "Kesehatan Masyarakat",
       "materi": "CKG dalam Integrasi Layanan Primer",
       "youtubeLink": "https://www.youtube.com/live/QBPhvNmrR6M?si=0dfm53BSaRzEsXrF",
+      "youtubeId": "QBPhvNmrR6M",
+      "foto": "https://img.youtube.com/vi/QBPhvNmrR6M/hqdefault.jpg",
       "tanggal": "13 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Scherly Hayer, S.Gz",
         "Donna P. Hutahean, SKM. M.Kes",
         "Host: Ni Made Oka Arpini Kasuma, SP., M.Kes"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 20,
       "kategori": "RSKD Jiwa Naimata",
       "materi": "Latar Belakang Mengakhiri Hidup Tanpa Stigma",
       "youtubeLink": "https://www.youtube.com/live/C41-0U0kg8w?si=39KDzBunskAMgMEd",
+      "youtubeId": "C41-0U0kg8w",
+      "foto": "https://img.youtube.com/vi/C41-0U0kg8w/hqdefault.jpg",
       "tanggal": "13 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr. Dennis Priscilla Setiono, Sp.KJ",
         "Host: Yusarmuni Reinnaty, S.Keb"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 21,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Bincang-bincang Terkait Praktik Baik Dalam Penanggulangan TBC Tingkat Provinsi",
       "youtubeLink": "https://youtu.be/ccoEP5ez1sY?si=z2Nyf2cec6g5Subr",
+      "youtubeId": "ccoEP5ez1sY",
+      "foto": "https://img.youtube.com/vi/ccoEP5ez1sY/hqdefault.jpg",
       "tanggal": "16 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -304,96 +326,103 @@ window.BAPOLES_DATA = {
         "Stef (Subkoordinator P2PM Kab. TTS)",
         "Deris (Kepala Desa Aulesa Kab. Lembata",
         "Host: Marianus Lino, SKM., M.Kes"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 22,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Sepekan Mengejar Imunisasi untuk Melengkapi Status Imunisasi Anak",
       "youtubeLink": "https://www.youtube.com/live/j1skuMri3io?si=43xzj83TnnEscoNA",
+      "youtubeId": "j1skuMri3io",
+      "foto": "https://img.youtube.com/vi/j1skuMri3io/hqdefault.jpg",
       "tanggal": "24 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Maria Anggreini, S.Farm.,Apt, MPH",
         "Suci Rahmawati",
         "Host: Maria F. Bukan, SKM, MKM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 23,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Kenali Kusta: Fakta Bukan Mitos",
       "youtubeLink": "https://youtu.be/ULKY-BflyJ4?si=8AO1qc82hYZb5SNy",
+      "youtubeId": "ULKY-BflyJ4",
+      "foto": "https://img.youtube.com/vi/ULKY-BflyJ4/hqdefault.jpg",
       "tanggal": "27 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Maratina D. Widayani, SKM",
         "dr. Ajra Fitri",
         "Host: Jasintha I. G. W. Ngongo, SKM, M.Kes"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 24,
       "kategori": "Pencegahan dan Pengendalian Penyakit",
       "materi": "Hepatitis: Kenali, Cegah, dan Deteksi Dini demi Masyarakat NTT yang Lebih Sehat, Perluas Akses, Perkuat Kolaborasi, Wujudkan Eliminasi Hepatitis",
       "youtubeLink": "https://www.youtube.com/live/RWpWefkwRW8?si=rmi5FaiNjIetGkPu",
+      "youtubeId": "RWpWefkwRW8",
+      "foto": "https://img.youtube.com/vi/RWpWefkwRW8/hqdefault.jpg",
       "tanggal": "28 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr. Nina Keraf, Sp.PD",
         "Host: Ni Made Oka Arpini Kasuma, SP., M.Kes"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 25,
       "kategori": "Kesehatan Masyarakat",
       "materi": "Kolaborasi GASING (Gereja Bersih Stunting) x LENTERA EMAS (Langkah Nyata Terpadu AtasiStunting Menuju Generasi Emas)",
       "youtubeLink": "https://www.youtube.com/live/PSojhMOUMx8?si=B6g2yi4ItxP5S8A1",
+      "youtubeId": "PSojhMOUMx8",
+      "foto": "https://img.youtube.com/vi/PSojhMOUMx8/hqdefault.jpg",
       "tanggal": "28 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Ruth D. Laiskodat, S.Si, Apt., MM",
         "Pdt. Gustaf Nenu (Pendeta GMIT Oevert, Fatululat, Amfoang Selatan",
         "Host: Raymundus V. Umbu Dagha, S.KM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 26,
       "kategori": "Pelayanan Kesehatan",
       "materi": "Bantuan Hidup Dasar (BHD) Langkah Cepat Menyelamatkan Nyawa",
       "youtubeLink": "https://www.youtube.com/live/zZumYi52Ob4?si=dayjT4FAyMwvyPpn",
+      "youtubeId": "zZumYi52Ob4",
+      "foto": "https://img.youtube.com/vi/zZumYi52Ob4/hqdefault.jpg",
       "tanggal": "30 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "dr. Andre Sinambela, Sp.EM",
         "Host: Ryska Sine"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 27,
       "kategori": "Pelatihan Tenaga Kesehatan",
       "materi": "Meng-upgrade Kompetensi Tenaga Kesehatan Bersama UPTD Pelatihan Tenaga Kesehatan Dinas Kesehatan Provinsi NTT",
       "youtubeLink": "https://www.youtube.com/live/LZMKoJwDlH4?si=any0p9QBVNLHIdoh",
+      "youtubeId": "LZMKoJwDlH4",
+      "foto": "https://img.youtube.com/vi/LZMKoJwDlH4/hqdefault.jpg",
       "tanggal": "31 Juli 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
         "Hermina R. Legimakani, S.Kep,Ners, MPH",
         "Ni Komang Nesawati, SKM, M.Kes",
         "Host: Rayana Saragih, ST"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 28,
       "kategori": "Tenaga Kesehatan",
       "materi": "Harmoni Pelayanan Sinergi Hak dan Kewajiban Tenaga Kesehatan, Tenaga Medis dan Fasilitas Pelayanan Kesehatan",
       "youtubeLink": "https://www.youtube.com/live/DKMQbx25TMY?si=HDGt4Yp4Az1OCjNp",
+      "youtubeId": "DKMQbx25TMY",
+      "foto": "https://img.youtube.com/vi/DKMQbx25TMY/hqdefault.jpg",
       "tanggal": "7 Agustus 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -401,14 +430,15 @@ window.BAPOLES_DATA = {
         "dr. Y.T. Kusumawardhani",
         "dr. Hery Bertus, MARS",
         "Host: Yusdistira R. Kikhau, SKM.,MKM"
-      ],
-      "foto": ""
+      ]
     },
     {
       "no": 29,
       "kategori": "Pelayanan Kesehatan",
       "materi": "Mengawal Generasi Emas NTT, Suara Muda dalam Porta Sapiens dan Riset Air Bersih",
       "youtubeLink": "https://www.youtube.com/live/zSkcu3-jXhM?si=FbbyXszV_H0JgWyj",
+      "youtubeId": "zSkcu3-jXhM",
+      "foto": "https://img.youtube.com/vi/zSkcu3-jXhM/hqdefault.jpg",
       "tanggal": "11 Agustus 2026",
       "waktu": "09.00 - 11.00 WITA",
       "narasumber": [
@@ -416,8 +446,7 @@ window.BAPOLES_DATA = {
         "Daren B.C. Tahun (Ketua Peneliti)",
         "Theresia C. Rea (Anggota Peneliti)",
         "Host: Secilia Lindawati Leo, S.Farm."
-      ],
-      "foto": ""
+      ]
     }
   ]
 };
